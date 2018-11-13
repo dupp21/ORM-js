a323a1c517a13835677ef78f1fbbff7a72ce2795
Domingos Bento Pereira <luakay@gmail.com>
2018-11-13 09:23:23 +0700

    Add files via upload

---
+++ api/products/controller.js
@@ -0,0 +1,15 @@
+const models = require("../models");
+
+exports.getAll = (req, res) => {
+  models.products
+    .findAll()
+    .then(products => res.send(products))
+    .catch(err => res.send(err));
+};
+
+exports.post = (req, res) => {
+  models.products
+    .create(req.body)
+    .then(product => res.send(product))
+    .catch(err => res.send(err));
+};
